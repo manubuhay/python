e0cd394bb285618ef46cb4abfb810522086483c0
Manu <Emmanuel@dnamicro.com>
2017-10-19 14:08:42 +0800

    added functions exercise

---
+++ functions.py
@@ -0,0 +1,15 @@
+
+def myfunction():
+	print("hello")
+	myName = "Emmanuel"
+	famName = input("What is your last name?")
+
+	if( famName == "Buhay" or famName == "buhay"):
+		print("True")
+	else:
+		print("You are gay")
+
+	print("\n")
+	print("Bye")
+
+myfunction()
