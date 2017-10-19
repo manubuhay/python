e19019288f6105ba3b9f5a404ff9be7b97f891c9
Manu <Emmanuel@dnamicro.com>
2017-10-19 15:21:17 +0800

    modified calculator.py for efficiency

--- calculator.py
+++ calculator.py
@@ -13,23 +13,18 @@ def divide(x, y):
 
 #Main function
 def main():
-	question = input("Bisaya ni na calculator, unsa imong ginahan buhaton?")
-	if(question == '+' or question == "dugangan"):
-		num1 = int(input("Gae kog numero: "))
-		num2 = int(input("Gae kog ikaduha na numero: "))
-		print(add(num1, num2))
-	elif(question == '-' or question == "minusan"):
-		num1 = int(input("Gae kog numero: "))
-		num2 = int(input("Gae kog ikaduha na numero: "))
-		print(subtract(num1, num2))
-	elif(question == '*' or question == "padaghanon"):
-		num1 = int(input("Gae kog numero: "))
-		num2 = int(input("Gae kog ikaduha na numero: "))
-		print(multiply(num1, num2))
-	elif(question == '/' or question == "tungaon"):
-		num1 = int(input("Gae kog numero: "))
-		num2 = int(input("Gae kog ikaduha na numero: "))
-		print(divide(num1, num2))
-	else:
+	question = input("Bisaya ni na calculator, unsa imong ginahan buhaton (+,-,*,/) ?")
+	if(question != '+' and question != '-' and question != '*' and question != '/'):
 		print("Pagtarung oi.")
+	else:
+		num1 = int(input("Gae kog numero: "))
+		num2 = int(input("Gae kog ikaduha na numero: "))
+		if(question == '-' or question == "minusan"):
+			print(subtract(num1, num2))
+		elif(question == '*' or question == "padaghanon"):
+			print(multiply(num1, num2))
+		elif(question == '/' or question == "tungaon"):
+			print(divide(num1, num2))
+		else:
+			print(add(num1, num2))
 main()
