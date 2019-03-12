f44aa45d27b7881b86cd59e0e9c7a0ed1f82fa5b
Manu Buhay <manubuhay@gmail.com>
2019-03-12 13:10:59 +0800

    translated to english

--- try_except.py
+++ try_except.py
@@ -14,9 +14,9 @@ def divide(x, y):
 #Main function
 def main():
 	try:
-		num1 = int(input("Gae kog numero: "))
-		num2 = int(input("Gae kog ikaduha na numero: "))
-		question = int(input("Bisaya ni na calculator, unsa imong ginahan buhaton (1 - Add,2 - Subtract,3 - Multiply, 4 - Divide?"))
+		num1 = int(input("Enter 1st number: "))
+		num2 = int(input("Enter 2nd number: "))
+		question = int(input("Choose your operation (1 - Add,2 - Subtract,3 - Multiply, 4 - Divide?"))
 	except:
 		print("Invalid Input!")
 		return #Exit program
