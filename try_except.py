d95b4b52d12db767af5b019fcf6b10cb04f2a7a0
Your Name <you@example.com>
2021-09-30 15:58:43 +0000

    fixed indentation and logic for try_except.py, add is called by number 1 and over 4

--- try_except.py
+++ try_except.py
@@ -1,31 +1,33 @@
 #Adds numbers
 def add(a, b):
-	return a + b
+    return a + b
 #Subtracts numbers
 def subtract(a, b):
-	return a - b
+    return a - b
 #Multiplies numbers
 def multiply(x, y):
-	return x * y
+    return x * y
 #Divides numbers
 def divide(x, y):
-	return x / y
+    return x / y
 
 #Main function
 def main():
 	try:
-		num1 = int(input("Enter 1st number: "))
-		num2 = int(input("Enter 2nd number: "))
-		question = int(input("Choose your operation (1 - Add,2 - Subtract,3 - Multiply, 4 - Divide?"))
+	    num1 = int(input("Enter 1st number: "))
+	    num2 = int(input("Enter 2nd number: "))
+	    question = int(input("Choose your operation( 1 - Add,2 - Subtract,3 - Multiply, 4 - Divide?)"))
 	except:
-		print("Invalid Input!")
-		return #Exit program
-	if(question == '2'):
-		print(subtract(num1, num2))
-	elif(question == '3'):
-		print(multiply(num1, num2))
-	elif(question == '4'):
-		print(divide(num1, num2))
+	    print("Invalid Input!")
+	    return #Exit program
+	if(question == 2):
+	    print(subtract(num1, num2))
+	elif(question == 3):
+	    print(multiply(num1, num2))
+	elif(question == 4):
+	    print(divide(num1, num2))
+        elif(question == 1):
+            print(add(num1,num2))
 	else:
-		print(add(num1, num2))
+		print("Number Out Of Range!")
 main()
