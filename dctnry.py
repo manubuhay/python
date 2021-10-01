f54f1bf0415933531c343325de25bb40ad19bdee
Your Name <you@example.com>
2021-10-01 08:55:41 +0000

    added dictionary and array exercises

---
+++ dctnry.py
@@ -0,0 +1,21 @@
+d = {}
+e = dict()
+
+#d["ct1"] = "1.1.1.62"
+
+d = {"ct1": "192.168.1.1", "ct2": "192.168.1.2"}
+d["ct3"] = "192.168.1.3"
+#print(d["ct1"])
+#print(d["ct2"])
+
+for i in d:
+    print(i)
+    print(d[i])
+
+
+#Input and output values into a dictionary
+index = raw_input("Input index: ")
+kypr = raw_input("Input keypair: ")
+e = {index: kypr}
+print("Index and keypair is: ")
+print(e)
---
+++ list.py
@@ -0,0 +1,10 @@
+x = [1,2,3,4,5]
+print(x)
+y = []
+
+max = input("How many values?")
+for i in range(0,max):
+    input1 = raw_input("Input values now: ")
+    y.append(input1)
+    #y.append(i) = input("Input values now: ")
+print(y)
