9439e061eb7415771bf2600d8a90411fb3254362
Your Name <you@example.com>
2021-10-02 06:07:32 +0000

    optimize code for swap exercise

--- swap_1st_last_char_string.py
+++ swap_1st_last_char_string.py
@@ -1,9 +1,9 @@
 #Swap 1st and last character of string length greater than 1
 def front_back(str):
   if len(str) > 1:
-    strt = str[0]
-    end = str[-1]
-    swapped = end + str[1:-1] + strt
+    #strt = str[0]
+    #end = str[-1]
+    swapped = str[-1] + str[1:-1] + str[0]
     return swapped
   return str
 
