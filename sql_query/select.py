e0dde3f0af27ff7e23847a1e13fae73c0d7f0ea4
python guy <python@python-59.1.1.1.56>
2021-10-07 09:06:05 +0000

    edited select.py to include new line on data display

--- sql_query/select.py
+++ sql_query/select.py
@@ -9,16 +9,16 @@ if conn.is_connected():
     cursor=conn.cursor();
     cursor.execute("show tables;") #No need for variable catcher
     tables=cursor.fetchall() #fetchall() retrieves data from last query(execute show tables), needs variable catcher
-    #for table in tables:
-    print(tables)
+    for i in tables: #Iterate over the list and display each values in new line
+        print(i)
     table=input("Which table? ")
     cmd="SELECT * FROM "
     query=cmd+table
     cursor.execute(query) #No need for variable catcher
     result=cursor.fetchall() #fetchall() retrieves data from last query(execute query), needs variable catcher
-    print(result)
-    #for i in result:
-    #    print(result)
+    #print(result) #Does not iterate, displays data with no new line
+    for i in result: #Iterate over the list and display each values in new line
+        print(i)
     cursor.close()
     conn.close()
 else:
