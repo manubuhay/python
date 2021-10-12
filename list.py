4352c4ab9c47ddd82a4a247f9203cb0b7e5ee3d3
python guy <python@python-59.1.1.1.56>
2021-10-12 17:46:21 +0000

    optimized list.py

--- list.py
+++ list.py
@@ -4,27 +4,27 @@ y = []
 
 max = int(input("How many values?"))
 for i in range(0,max):
-    input1 = input(str(i+1) + ".) " + "Input values now: ")
-    y.append(input1)
-    #y.apnpend(i) = input("Input values now: ") #Cannot assign value to function, use variables instead
+    input1 = int(input(str(i+1) + ".) " + "Input values now: ")) #Convert to int because default is string type
+    y.append(input1) #If int is not specified, list will be sorted according to 1st digit
+    #y.append(i) = input("Input values now: ") #Cannot assign value to function, use variables instead
 print(y,"\n")
 
 #Execute selection sort(Descending)
 for i in range(max-1):
     for j in range(i+1,max):
-        if int(y[i]) > int(y[j]): #Convert to int because list is string type
-            temp = int(y[j]) #If int is not specified, list will be sorted according to 1st digit
-            y[j] = int(y[i])
-            y[i] = int(temp)
+        if y[i] > y[j]:
+            temp = y[j]
+            y[j] = y[i]
+            y[i] = temp
 print("Descending order:")
 print(y,"\n")
 
 #Execute selection sort(Ascending)
 for i in range(max-1):
     for j in range(i+1,max):
-        if int(y[i]) < int(y[j]): #Convert to int because list is string type
-            temp = int(y[j]) #If int is not specified, list will be sorted according to 1st digit
-            y[j] = int(y[i])
-            y[i] = int(temp)
-print("Ascending order:")
-print(y)
+        if y[i] < y[j]:
+            temp = y[j]
+            y[j] = y[i]
+            y[i] = temp
+print("Descending order:")
+print(y,"\n")
