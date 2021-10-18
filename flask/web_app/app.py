133c38947be1b85ca9b7c0210fbfec9b15beab65
Manu Buhay <manubuhay@gmail.com>
2021-10-18 14:45:07 +0000

    added profile and logout route, html template inheritance

--- flask/web_app/app.py
+++ flask/web_app/app.py
@@ -1,21 +1,27 @@
-from flask import Flask, request, render_template,redirect
+from flask import Flask,request,render_template,redirect,session,url_for
 import os
 import mysql.connector as dtb
+from datetime import timedelta
+
 mydb=dtb.connect(host="1.1.1.59"
                 ,user="python"
                 ,password="Python_123"
                 ,database="socmed")
 
 app=Flask(__name__)
+app.secret_key="!@#$%"
+app.permanent_session_lifetime= timedelta(hours=5)
 
 @app.route("/")
 def index_page():
     return render_template("index.html")
 
 @app.route("/", methods=["POST"])
-def logged_in():
+def log_in():
+    session.permanent=True # Reinforces line 13, false logs you out when exiting browser/tab
     uid=request.form['utag']
     passwd=request.form['pass']
+    session['user_id']=uid
 
     if mydb.is_connected():
         cursor=mydb.cursor()
@@ -24,8 +30,11 @@ def logged_in():
         cursor.execute(query,data)
         results=cursor.fetchall()
         if len(results)==1:
+            #return redirect(url_for("user_profile"))
             return render_template("profile.html")
         else:
+            if "user_id" in session:
+                return redirect(url_for("/profile"))
             return redirect("/registration")
     return "Error connecting to database!"
 
@@ -47,6 +56,19 @@ def register_now():
             mydb.close()
             return redirect("/")
     return render_template("register.html")
+
+@app.route("/profile")
+def user_profile():
+    if  "user_id" in session:
+        user=session["user"]
+        #return "Welcome "+ f"<h1>{user}!</h1>"
+        return render_template("profile.html")
+    return redirect("/")
+
+@app.route("/logout")
+def log_out():
+    session.pop("user_id",None)
+    return redirect(url_for("log_in"))
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port="1234",debug=True)
