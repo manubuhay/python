152c8c9ea37a006249c95fb21d780b5b31d9b337
Manu Buhay <manubuhay@gmail.com>
2021-10-19 11:51:01 +0000

    added css templates from designseer, functionized insert and  if user exists in sq, removed if session statements in app.py

--- flask/web_app/app.py
+++ flask/web_app/app.py
@@ -12,30 +12,46 @@ app=Flask(__name__)
 app.secret_key="!@#$%"
 app.permanent_session_lifetime= timedelta(hours=5)
 
-@app.route("/")
-def index_page():
-    if "user_id" in session:
-        return redirect(url_for("user_profile")) #Redirect user to profile page if session exists
-    return render_template("index.html")
+# @app.route("/")
+# def index_page():
+#     if "user_id" in session:
+#         return redirect(url_for("user_profile")) #Redirect user to profile page if session exists
+#     return render_template("index.html")
 
-@app.route("/", methods=["POST"])
-def log_in():
-    session.permanent=True # Reinforces line 13, false logs you out when exiting browser/tab
-    uid=request.form['utag']
-    passwd=request.form['pass']
-    session['user_id']=uid
-
+def if_exists(tag,pw):
     if mydb.is_connected():
         cursor=mydb.cursor()
         query="""SELECT usertag, passwd FROM human WHERE usertag=%s AND passwd=%s"""
-        data=(uid,passwd)
+        data=(tag,pw)
         cursor.execute(query,data)
         results=cursor.fetchall()
+        cursor.close()
+        return results
+
+def insert_db(name,passwd,eadd,utag):
+    if mydb.is_connected():
+        cursor=mydb.cursor()
+        query="""INSERT INTO human(name,passwd,email,usertag) VALUES( %s, %s, %s, %s)"""
+        data=(name,passwd,eadd,utag)
+        cursor.execute(query,data)
+        mydb.commit()
+        cursor.close()
+
+@app.route("/", methods=["POST","GET"])
+def log_in():
+    if request.method=="POST":
+        session.permanent=True # Reinforces line 13, false logs you out when exiting browser/tab
+        uid=request.form['utag']
+        passwd=request.form['pass']
+        session['user_id']=uid
+        session['pwd']=passwd
+        results=if_exists(uid, passwd)
         if len(results)==1:
             return redirect(url_for("user_profile"))
         else:
             return redirect("/registration")
-    return "Error connecting to database!"
+    else:
+         return render_template("index.html")
 
 @app.route("/registration", methods=["GET", "POST"])
 def register_now():
@@ -44,25 +60,14 @@ def register_now():
         passwd=request.form['pass']
         usertag=request.form['utag']
         eadd=request.form['e_mail']
-
-        if mydb.is_connected():
-            cursor=mydb.cursor()
-            query="""INSERT INTO human(name,passwd,email,usertag) VALUES( %s, %s, %s, %s)"""
-            data=(uname,passwd,eadd,usertag)
-            cursor.execute(query,data)
-            mydb.commit()
-            cursor.close()
-            #mydb.close()
-            return redirect("/")
-    else:
-        if "user_id" in session: #If session exists, make '/registration' inaccessible to logged-in user
-            return redirect(url_for("user_profile"))
+        insert_db(uname,passwd,eadd,usertag)
+        return redirect("/")
     return render_template("register.html")
 
 @app.route("/profile")
 def user_profile():
     if "user_id" in session:
-        user=session["user"]
+        user=session["user_id"]
         #return "Welcome "+ f"<h1>{user}!</h1>"
         return render_template("profile.html")
     return redirect("/")
@@ -71,6 +76,20 @@ def user_profile():
 def log_out():
     session.pop("user_id",None)
     return redirect(url_for("log_in"))
+
+@app.route("/delete")
+def delete_user():
+    if "user_id" in session:
+        if mydb.is_connected():
+            query="""DELETE FROM human WHERE usertag=%s"""
+            data=(session['user_id'],)
+            cursor=mydb.cursor()
+            cursor.execute(query,data)
+            mydb.commit()
+            cursor.close()
+        else:
+            return "Error connecting to database!"
+    return redirect("/")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port="1234",debug=True)
