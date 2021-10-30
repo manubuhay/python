31163880c2ddae2283b6e546f9a7bdb550f4b36f
Manu Buhay <manubuhay@gmail.com>
2021-10-30 07:09:12 +0000

    turned sql functions as a module, removed comments

--- flask/web_app/app.py
+++ flask/web_app/app.py
@@ -1,82 +1,25 @@
 from flask import Flask,request,render_template,redirect,session,url_for
 import os
-import mysql.connector as dtb
 from datetime import timedelta
-
-mydb=dtb.connect(host="codebase"
-                ,user="python"
-                ,password="Python_123"
-                ,database="socmed")
+import query_db as qDB
 
 app=Flask(__name__)
 app.secret_key="!@#$%"
 app.permanent_session_lifetime= timedelta(hours=5)
-
-################################################################################
-def ifexists_db(utag,pw):
-    if mydb.is_connected():
-        cursor=mydb.cursor()
-        query="""SELECT usertag, passwd FROM user_data WHERE usertag=%s AND passwd=%s"""
-        data=(utag,pw)
-        cursor.execute(query,data)
-        #results=cursor.fetchall()
-        results=cursor.fetchone()
-        cursor.close()
-        return results
-
-def insert_db(name,passwd,eadd,utag):
-    if mydb.is_connected():
-        cursor=mydb.cursor()
-        query="""INSERT INTO user_data(name,passwd,email,usertag) VALUES( %s, %s, %s, %s)"""
-        data=(name,passwd,eadd,utag)
-        cursor.execute(query,data)
-        mydb.commit()
-        cursor.close()
-
-def remove_db(utag):
-    if mydb.is_connected():
-        cursor=mydb.cursor()
-        query="""DELETE FROM user_data WHERE usertag=%s"""
-        data=(utag,)
-        cursor.execute(query,data)
-        mydb.commit()
-        cursor.close()
-
-def update_db(name,passwd,eadd,utag,old_utag):
-    if mydb.is_connected():
-        cursor=mydb.cursor()
-        query="""UPDATE user_data SET name=%s, passwd=%s, email=%s, usertag=%s WHERE usertag=%s"""
-        data=(name,passwd,eadd,utag,old_utag)
-        cursor.execute(query,data)
-        mydb.commit()
-        cursor.close()
-
-def fetchuser_db(utag):
-    if mydb.is_connected():
-        cursor=mydb.cursor()
-        query="""SELECT * FROM user_data WHERE usertag=%s"""
-        data=(utag,)
-        cursor.execute(query,data)
-        results=cursor.fetchone()
-        return results
-################################################################################
 
 @app.route("/", methods=["POST","GET"])
 def log_in():
     err_msg=""
     if request.method=="POST":
         if all((request.form['utag'],request.form['pass'])):
-        #if request.form['utag'] and request.form['pass']:
-            session.permanent=True # Reinforces line 13, false logs you out when exiting browser/tab
+            session.permanent=True # Reinforces line 8, false logs you out when exiting browser/tab
             uid=request.form['utag']
             passwd=request.form['pass']
-            results=ifexists_db(uid, passwd)
-            #if len(results)==1:
+            results=qDB.ifexists_db(uid, passwd)
             if results: # If authenticated, save session data, if declared out of this
                 session['user_id']=uid # "if statement", it will save POST data and will direct you to /profile
                 return redirect(url_for("user_profile",user=session['user_id'])) # even if user doesn't exist in DB
             return redirect("/registration")
-        #return "Missing Value/s!"
         err_msg="Missing Values!"
     else:
         if "user_id" in session:
@@ -92,10 +35,9 @@ def register_now():
         usertag=request.form['utag']
         eadd=request.form['e_mail']
         if not all((uname,passwd,usertag,eadd)): #Makes sure all form inputs are not empty
-            #return "Missing Values!"
             err_msg="Value/s are missing!"
         else:
-            insert_db(uname,passwd,eadd,usertag) #Inserts values if none are empty
+            qDB.insert_db(uname,passwd,eadd,usertag) #Inserts values if none are empty
             return redirect("/")
     else:
         if "user_id" in session:
@@ -105,9 +47,7 @@ def register_now():
 @app.route("/profile/<user>")
 def user_profile(user):
     if "user_id" in session:
-        user=session["user_id"]
-        #return "Welcome "+ f"<h1>{user}!</h1>"
-        return render_template("profile.html",user=session['user_id']) # Pass user as parameter to diplay in HTML
+        return render_template("profile.html",user=session["user_id"]) # Pass user as parameter to diplay in HTML
     return redirect("/")                                               # page, you can also just use session data
 
 @app.route("/logout")
@@ -118,7 +58,7 @@ def log_out():
 @app.route("/delete")
 def delete_user():
     if "user_id" in session:
-        remove_db(session['user_id']) # Remove user in DB first before popping session otherwise parameter
+        qDB.remove_db(session['user_id']) # Remove user in DB first before popping session otherwise parameter
         session.pop("user_id",None)   # in remove_db will be empty, "None" will prevent raising error
     else:                             # keyError if the key at the first parameter is not found in the session
         return "Error connecting to database!"
@@ -127,7 +67,7 @@ def delete_user():
 @app.route("/update", methods=["POST","GET"])
 def update_user(): # "usertag" will be readonly in "update_details.html", to preserve session data
     if "user_id" in session:
-        results=fetchuser_db(session['user_id'])
+        results=qDB.fetchuser_db(session['user_id'])
         if request.method=="POST":
             uname=request.form['up_name']
             passwd=request.form['up_pass']
@@ -136,8 +76,8 @@ def update_user(): # "usertag" will be readonly in "update_details.html", to pre
             if not all((uname,passwd,usertag,eadd)):
                 return "Values cannot be empty!"
             else:
-                update_db(uname,passwd,eadd,usertag,session['user_id'])
-                results=fetchuser_db(session['user_id']) # Pass values to display on "update_details.html"
+                qDB.update_db(uname,passwd,eadd,usertag,session['user_id'])
+                results=qDB.fetchuser_db(session['user_id']) # Pass values to display on "update_details.html"
         return render_template("update_details.html",results=results)
     else:
         return redirect(url_for("log_in"))
