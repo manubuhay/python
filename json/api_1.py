aa65b92e48be8b2a83da4aac56458a9d8296bde2
Manu Buhay <manubuhay@gmail.com>
2021-10-30 17:26:07 +0000

    added json

---
+++ json/api_1.py
@@ -0,0 +1,20 @@
+import json
+from urllib.request import urlopen
+# From "https://github.com/herval/yahoo-finance/issues/51" and "https://github.com/herval/yahoo-finance/issues/51"
+# with urlopen("https://query1.finance.yahoo.com/v7/finance/quote?symbols=TLS.AX,MUS.AX") as response:
+with urlopen("https://query1.finance.yahoo.com/v8/finance/chart/GOOG?range=1d&includePrePost=false&interval=2m&corsDomain=finance.yahoo.com&.tsrc=finance") as response:
+    source = response.read()
+
+# print(source)
+data = json.loads(source) # Convert to python format
+# print(json.dumps(data['chart']['result'][0]['timestamp'],indent=4))
+print(len(data['chart']['result'][0]['timestamp']))
+
+# usd_rates = dict()
+#
+# for item in data['list']['resources']:
+#     name = item['resource']['fields']['name']
+#     price = item['resource']['fields']['price']
+#     usd_rates[name] = price
+#
+# print(50 * float(usd_rates['USD/INR']))
---
+++ json/json_loads_dumps.py
@@ -0,0 +1,33 @@
+''' JavaScript Object Notation'''
+import json
+
+json_string = '''
+{
+    "people": [
+        {
+            "name": "john smith",
+            "phone": "615-555-7164",
+            "email": ["john@bogus.com", "jsmith@place.com"],
+            "has_license": false
+        },
+        {
+            "name": "jane doe",
+            "phone": "560-555-7164",
+            "email": null,
+            "has_license": true
+        }
+             ]
+}
+'''
+data=json.loads(json_string)
+# print(data)
+# print(type(data))
+# print(data['people'])
+print(type(data['people'])) #Returns a type list
+for person in data['people']:
+    #print(person)
+    # print(person['phone'])
+    del person['phone'] # Delete phone key in json file
+
+new_data=json.dumps(data,indent=2, sort_keys=True) # Use indent parameter to make output readable
+print(new_data)
