f21b116a50ac21e32fa7a3438e22548b44726171
Manu Buhay <manubuhay@gmail.com>
2022-06-28 17:01:23 +0800

    added upload local file feature

--- speech_to_text/assembly_ai/assembly.py
+++ speech_to_text/assembly_ai/assembly.py
@@ -30,6 +30,25 @@ class AssemblyAI:
         response=requests.post(self.BASE_URL+'transcript',headers=self.headers,json=request_body)
         return response
 
+    def upload_audio_by_file(self,audio_file_path,chunk_size=5241880,remove_filler_word=True,format_text=True, **kwarg):
+        if not os.path.exists(audio_file_path) or not os.path.isfile(audio_file_path):
+            print("FIle is not found.")
+            return
+        def read_file(file_path,chunk_size=chunk_size):
+            with open(file_path,mode='rb') as _file:
+                while True:
+                    data=_file.read(chunk_size)
+                    if not data:
+                        break
+                    yield data
+        audio_data=read_file(audio_file_path)
+        headers=self.headers
+        headers['content-type']='application/json'
+        upload_response=requests.post(self.BASE_URL+"upload",headers=headers,data=audio_data)
+
+        response=self.upload_audio_by_url(upload_response.json()['upload_url'],remove_filler_word=remove_filler_word,format_text=format_text, **kwarg)
+        return response
+
     def retrieve_transcript(self,transcript_id):
         response=requests.get(self.BASE_URL+'transcript/'+transcript_id,headers=self.headers)
         return response.json()
