c705b9d01595b8b22a310b6d0db2bb24a47044dc
Manu Buhay <manubuhay@gmail.com>
2023-04-13 15:26:52 +0000

    removed sensitive info

---
+++ sql_query/sql_to_html/table_values.py
@@ -0,0 +1,23 @@
+import mysql.connector as db
+import os
+import pandas as pd
+
+conn=db.connect(host="<ip_of_db>"
+                ,username="python"
+                ,password="Python_123"
+                ,database="sanctuary")
+
+#Fetch data
+cmd="select * from monsters"
+cursor=conn.cursor()
+cursor.execute(cmd)
+data=cursor.fetchall()
+# print(data)
+
+#Manipulate data
+df=pd.DataFrame()
+for x in data:
+    df2=pd.DataFrame(list(x)).T
+    df=pd.concat([df,df2])
+
+df.to_html('templates/data.html')
