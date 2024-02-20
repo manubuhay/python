2d3418feba2c8e677e63dc28bc1fc333b8dd0378
Manu Buhay <manubuhay@gmail.com>
2024-02-20 17:44:22 +0800

    added multiple reboot script

---
+++ paramiko/reboot_multiple.py
@@ -0,0 +1,20 @@
+#Only use when Python version >= 3.8x
+import paramiko
+import sys
+
+def send_command(cmd,servers):
+    for i in servers:
+        client=paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.connect(i["ip"],username=i["user"],password=i["password"]) # You can also use ssh-copy-id(or copy client's id_rsa.pub to server's auth_keys), to omit password as parameter
+        ssh_stdin,ssh_stdout,ssh_stderr=client.exec_command(cmd)
+
+def main():
+    servers = [
+    {"ip": "IP1", "user": "root","password": "password"},
+    {"ip": "IP2", "user": "root","password": "password"}
+    ]
+    cmd="reboot"
+    send_command(cmd,servers)
+
+main()
