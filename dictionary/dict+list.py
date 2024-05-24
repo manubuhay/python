38a3b60d1842a97f629d8f497a63a9662b91f1c8
Manu Buhay <manubuhay@gmail.com>
2024-05-24 16:21:42 +0800

    added list of dictionary iteration, and dict files placed in 1 directory

---
+++ dictionary/dict+list.py
@@ -0,0 +1,50 @@
+# company={
+#     "name":"john smith",
+#     "designation":"IT",
+#     "age":35,
+#     "location":(12.34567,-98.7654)
+# }
+# print(atox["name"])
+# print(atox["location"])
+
+companylist=[
+    {
+    "name":"john smith",
+    "designation":"IT",
+    "age":25,
+    "location":(12.34567,-98.7654)
+    },
+    {
+    "name":"jane doe",
+    "designation":"HR",
+    "age":25,
+    "location":(32.34567,-87.7654)
+    },
+    {
+    "name":"mark smith",
+    "designation":"CEO",
+    "age":45,
+    "location":(32.34567,-87.7654)
+    },
+    {
+    "name":"jack sparrow",
+    "designation":"Accountant",
+    "age":30,
+    "location":(42.34567,-77.7654)
+    }
+            ]
+
+# print(companylist[2]["designation"])
+# print(companylist[1]["location"])
+# print(companylist[0]["age"])
+
+# print(companylist)
+for x in range(len(companylist)): # x must be int to hold index of list companylist, use "len" method to get length of companylist
+    for y in companylist[x]: 
+        if companylist[x][y] == 25: # print records that have 25 as age
+            print(companylist[x])
+
+for a in range(len(companylist)):
+    for b in companylist[a]:
+        if "location" in b: # if location exists as key pair, print dictionary record
+            print(companylist[a][b])
