96fd0c7c8b6246a80e889f0625c5d23e0218967d
Manu Buhay <manubuhay@gmail.com>
2024-07-06 21:40:15 +0800

    added link to source website of bypassing warning during initial ssh session

--- paramiko/reboot_multiple.py
+++ paramiko/reboot_multiple.py
@@ -6,6 +6,7 @@ def send_command(cmd,servers):
     for i in servers:
         client=paramiko.SSHClient()
         client.load_system_host_keys()
+        # From: https://techoverflow.net/2022/01/23/how-to-fix-paramiko-sshexception-server-not-found-in-known_hosts/
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # Bypasses the "The authenticity of host 'x.x.x.x' can't be established."
         client.connect(i["ip"],username=i["user"],password=i["password"]) # You can also use ssh-copy-id(or copy client's id_rsa.pub to server's auth_keys), to omit password as parameter
         ssh_stdin,ssh_stdout,ssh_stderr=client.exec_command(cmd)
