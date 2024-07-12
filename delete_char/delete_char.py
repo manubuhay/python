dc8947d255b1d5dcf0983f1265784f4160fe91da
Manu Buhay <manubuhay@gmail.com>
2024-07-12 14:03:15 +0000

    added automated delete character in a file

---
+++ delete_char/delete_char.py
@@ -0,0 +1,8 @@
+mac_file = list(open('sample.txt'))
+new_mac = []
+for i in range(len(mac_file)):
+    # print(mac_list[i].strip('\n'))
+    new_mac.append(mac_file[i].replace(":",""))
+for i in range(len(new_mac)):
+    with open('removed_char.txt', 'a') as newfile:
+        newfile.write(new_mac[i])
