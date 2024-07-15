d4e1c1231a61492c92e605ef8a681b5b89ccf622
Manu Buhay <manubuhay@gmail.com>
2024-07-15 14:08:44 +0800

    commented redundant loop

--- delete_char/delete_char.py
+++ delete_char/delete_char.py
@@ -3,6 +3,6 @@ new_mac = []
 for i in range(len(mac_file)):
     # print(mac_list[i].strip('\n'))
     new_mac.append(mac_file[i].replace(":",""))
-for i in range(len(new_mac)):
+#for i in range(len(new_mac)):
     with open('removed_char.txt', 'a') as newfile:
         newfile.write(new_mac[i])
